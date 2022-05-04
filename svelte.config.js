352e91aba6b2c290677bf40b781bd56ea8660b9a
Alejandro Sáez <alejandro.sapa@gmail.com>
2022-05-04 11:33:43 -0500

    feat: Second commit

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			edge: true
+		})
 	},
 
 	preprocess: [
