d0d10041ebea80f47222c20f480688367938a194
Alejandro Sáez <alejandro.sapa@gmail.com>
2022-06-07 13:20:16 -0400

    Se agrego imagen de agradecimiento

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -1,10 +1,10 @@
-const analyticsTracking = (eventName) => {
-    if(typeof window !=== undefined){
-        window.ga('send', 'event', 'flow', 'click', eventName){
-            nonInteraction: true
+// const analyticsTracking = (eventName) => {
+//     if(typeof window !=== undefined){
+//         window.ga('send', 'event', 'flow', 'click', eventName){
+//             nonInteraction: true
 
-        }
-    }
-}
+//         }
+//     }
+// }
 
-export default analyticsTracking;
+// export default analyticsTracking;
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,70 +1,11 @@
 <script>
-	import analyticsTracking from '$lib/analytics';
+
 </script>
 
 <svelte:head>
 	<title>5LC - Bóveda 3.0</title>
 </svelte:head>
 
-<div class="w-full md:w-9/12 lg:w-6/12 m-auto mt-10">
-	<div class="flex justify-center mb-6">
-		<img class="w-28" src="/logo.png" alt="Logotipo de 5 Luchas Clandestino" />
-	</div>
-	<div class="text-center">
-		<span class="text-xl text-accent">¡SIEMPRE BUENA ONDA!</span>
-		<h1 class="mb-2 text-primary text-4xl font-bold">Bóveda Secreta 3.0</h1>
-	</div>
-	<div class="p-6 prose prose-p:text-lg">
-		<p>
-			Juntos como comunidad Clandestino vivimos momentos inolvidables; fuimos testigos de intensos
-			combates e instancias donde conocimos nuevos amig@s, también del crecimiento de la
-			organización y cosas que convirtieron a la Bóveda Secreta en un lugar especial y único.
-		</p>
-		<p>
-			Ya llevamos más de 2 años sin Bóveda; 2 años sin acción dentro del ring, 2 años sin sentir esa
-			energía que en conjunto generábamos, pero...
-		</p>
-		<p>
-			<span class="text-primary">¡Hoy es momento de volver a intentarlo! </span> Queremos volver a generar
-			este espacio y comunidad únicos, pero no podemos solos; somos honestos y transparentamos que necesitamos
-			del apoyo de la comunidad. Es por eso que iniciamos una campaña de aportes voluntarios, con el
-			fin de reconstruir lo que tod@s tanto queremos. Abajo encontrarán un link donde podrán hacer un
-			aporte voluntario a la causa; cada granito aportará, y lo agradecemos de todo corazón.
-		</p>
-		<p>
-			Con esto intentaremos encontrar un lugar que se adapte a las necesidades de los espectáculos y
-			generar un lugar de entrenamiento, que mantenga los estándares a los que 5 Luchas Clandestino
-			siempre ha apuntado.
-		</p>
-
-		<p class="text-primary">
-			Agradecemos tu aporte y esperamos pronto volver a vernos, en ese espacio que tanto extrañamos.
-			✌️
-		</p>
-	</div>
+<div class="w-full h-screen">
+	<img class="object-contain h-screen w-full" src="/gracias.jpg" alt="Imagen de agradecimiento por el apoyo entregado." />
 </div>
-<div class="w-full md:w-9/12 lg:w-6/12 m-auto">
-	<a
-		on:click={analyticsTracking('flow-btn')}
-		class="flex justify-center mb-4"
-		href="
-	https://www.flow.cl/btn.php?token=qf7khk9"
-		target="_self"
-	>
-		<img src="/btn-flow.png" alt="" />
-	</a>
-
-	<iframe
-		title="5 Luchas Clandestino"
-		class="w-full aspect-video"
-		src="https://www.youtube.com/embed/ak7LbwJhTBI?start=21"
-	/>
-</div>
-
-<div class="fixed w-28 md:w-60 md:h-60 bottom-0 right-10 md:right-0 lg:right-36 z-10">
-	<img class="top-0" src="/bovedin-icon.png" alt="" />
-</div>
-
-<footer class="w-full">
-	<img class="w-full grayscale" src="/boveda.png" alt="Boveda Secreta" />
-</footer>
