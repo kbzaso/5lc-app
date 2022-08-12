ff11a9a6a151004e5c012897403ae2365523fd41
Alejandro Sáez <alejandro.sapa@gmail.com>
2022-08-12 10:41:53 -0400

    No event announcement

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Important from '../components/Important.svelte';
 </script>
 
 <svelte:head>
@@ -7,30 +6,20 @@
 </svelte:head>
 
 <div class="w-fit mx-auto hero min-h-screen bg-base-200">
-	<div class="hero-content flex-col lg:flex-row w-5/6">
-		<img
-			src="/afiche-julio.jpg"
-			alt="Afiche del evento"
-			class="sm:max-w-sm self-start rounded-lg shadow-2xl"
-		/>
+	<div class="hero-content flex-col lg:flex-row w-4/6 text-xl">
 		<div>
-			<h1 class="text-5xl text-primary font-bold">No estamos muertos</h1>
+			<h1 class="text-5xl text-primary font-bold">Siempre buena onda</h1>
+			<p class="pt-6">En Julio demostramos que no estamos muertos ¡y de que manera! 🔥</p>
 			<p class="pt-6">
-				Pandemias, guerras, cambio climatico. Nada ni nadie nos asegura la vida, por eso la
-				invitacion es a vivir el momento, a ser libre y feliz.
+				La energía de la Bóveda se hizo sentir tal cómo en sus orígenes y queremos que eso se
+				mantenga. Con el fin de procurar un mejor servicio para todos, <span
+					class=" underline decoration-primary font-bold"
+					>no tendremos eventos hasta nuevo aviso</span
+				>, para así ocuparnos de aquellas consideraciones que harán de este un espacio más
+				profesional y seguro para todos.
 			</p>
-			<p class="py-6">
-				Y porque la lucha nos hace libres, volvemos a intentarlo viernes tras viernes por todo
-				julio.
-			</p>
-
-			<h2 class="text-lg font-bold mb-4 underline text-primary">Ubicación y horarios:</h2>
-			<p>¿Donde? en la Bóveda Secreta - San Antonio #705</p>
-			<p>Apertura de puertas: 19:15 hrs.</p>
-			<p>Inicio de evento: 20:00 hrs.</p>
-			<Important />
-			<h2 class="text-lg font-bold mb-4 underline text-primary">Adquiere tu entradas:</h2>
-			<p class=" text-red-500">Entradas agotadas</p>
+			<p class="py-6">Gracias por todo su apoyo 💛</p>
+			<img class="w-32 mb-4" src="/logo.png" alt="Logotipo 5LC" />
 		</div>
 	</div>
 </div>
