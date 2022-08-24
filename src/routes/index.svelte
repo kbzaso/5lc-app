f3266e23676a524ab9d967e76c8cd20379f311f3
Alejandro Sáez <alejandro.sapa@gmail.com>
2022-08-24 19:04:57 -0400

    fixing display styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,19 +7,20 @@
 </svelte:head>
 
 <div class="w-fit mx-auto hero min-h-screen bg-base-200">
-	<div class="hero-content flex-col lg:flex-row w-5/6">
+	<div class="hero-content flex-col lg:flex-row md:w-5/6 w-full">
 		<img
 			src="/afiche.png"
 			alt="Afiche del evento"
-			class="sm:max-w-sm self-start rounded-lg shadow-2xl"
+			class="sm:max-w-lg lg:self-start rounded-lg shadow-2xl"
 		/>
-		<div>
-			<h1 class="text-5xl text-primary font-bold uppercase">Pasamos agosto</h1>
-			<p class="pt-6">
-				Ya estamos casi casi, último viernes de agosto y ¡Que viernes! Ya que celebraremos 5 años
-				del inicio de esta locura, locura que nos ha llenado de alegrias, frustraciones, lesiónes,
-				cañas pero sobre todo de <span class="underline decoration-primary decoration-2"
-					>buena onda.</span
+		<div class="">
+			<h1 class="text-5xl text-primary font-bold uppercase mt-2 sm:hidden md:block">
+				Pasamos agosto
+			</h1>
+			<p class="py-6">
+				Ya estamos casi, último viernes de agosto y ¡Qué viernes! Ya que celebraremos 5 años del
+				inicio de esta locura, locura que nos ha llenado de alegrías, frustraciones, lesiones, cañas
+				pero sobre todo de <span class="underline decoration-primary decoration-2">buena onda.</span
 				>
 			</p>
 
@@ -32,7 +33,7 @@
 			<!-- <p class=" text-red-500">Entradas agotadas</p> -->
 			<button
 				on:click={() => (window.location.href = 'https://www.flow.cl/btn.php?token=ajzvzpx')}
-				class="btn btn-primary">26 de agosto</button
+				class="btn btn-primary w-full md:w-fit">26 de agosto</button
 			>
 		</div>
 	</div>
