9128bf8cd0075337661bcd424a02dfe0490ca638
Alejandro Sáez <alejandro.sapa@gmail.com>
2022-08-24 18:11:04 -0400

    aniversario 2022

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,19 +9,20 @@
 <div class="w-fit mx-auto hero min-h-screen bg-base-200">
 	<div class="hero-content flex-col lg:flex-row w-5/6">
 		<img
-			src="/afiche-julio.jpg"
+			src="/afiche.png"
 			alt="Afiche del evento"
 			class="sm:max-w-sm self-start rounded-lg shadow-2xl"
 		/>
 		<div>
-			<h1 class="text-5xl text-primary font-bold">No estamos muertos</h1>
+			<h1 class="text-5xl text-primary font-bold uppercase">Pasamos agosto</h1>
 			<p class="pt-6">
-				Pandemias, guerras, cambio climatico. Nada ni nadie nos asegura la vida, por eso la
-				invitacion es a vivir el momento, a ser libre y feliz.
+				Ya estamos casi casi, último viernes de agosto y ¡Que viernes! Ya que celebraremos 5 años
+				del inicio de esta locura, locura que nos ha llenado de alegrias, frustraciones, lesiónes,
+				cañas pero sobre todo de buena onda.
 			</p>
 			<p class="py-6">
-				Y porque la lucha nos hace libres, volvemos a intentarlo viernes tras viernes por todo
-				julio.
+				Porque no esta muerto el que lucha, seguimos intentandolo, contra viento y marea la bóveda
+				secreta abre sus puertas a sus miembros.
 			</p>
 
 			<h2 class="text-lg font-bold mb-4 underline text-primary">Ubicación y horarios:</h2>
@@ -30,7 +31,8 @@
 			<p>Inicio de evento: 20:00 hrs.</p>
 			<Important />
 			<h2 class="text-lg font-bold mb-4 underline text-primary">Adquiere tu entradas:</h2>
-			<p class=" text-red-500">Entradas agotadas</p>
+			<button class="btn btn-primary">26 de agosto</button>
+			<!-- <p class=" text-red-500">Entradas agotadas</p> -->
 		</div>
 	</div>
 </div>
