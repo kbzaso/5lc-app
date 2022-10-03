8f93c589422abf6d888970eedf9d20b5081a02ba
Alejandro Sáez <alejandro.sapa@gmail.com>
2022-10-03 15:36:15 -0300

    Device detector

--- src/components/Video.svelte
+++ src/components/Video.svelte
@@ -1,7 +1,17 @@
-
+<script>
+  import DeviceDetector from "svelte-device-detector";
+</script>
 <div class="w-full">
   <div class=" max-w-4xl mx-auto md:py-24 pb-10">
-    <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/756500028?h=b5c9247606&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" class="w-full aspect-video" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="1era confirmada para OCTUBRE ROJO 2022"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+
+    <DeviceDetector showInDevice="desktop">
+      <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/756500028?h=b5c9247606&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" class="w-full aspect-video" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Primera confirmada para OCTUBRE ROJO 2022"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+    </DeviceDetector>
+    
+    <DeviceDetector showInDevice="mobile">
+      <div style="padding:177.78% 0 0 0;position:relative;"><iframe class="aspect-auto" src="https://player.vimeo.com/video/756508531?h=fa855729c2&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Primera confirmada para OCTUBRE ROJO 2022"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+    </DeviceDetector>
+    
   </div>
 </div>
   
