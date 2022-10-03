4fd0d4b8c1bbd22c519d8fd76cc54dd56e080d94
Alejandro Sáez <alejandro.sapa@gmail.com>
2022-10-03 15:14:25 -0300

    Kristy annousment

--- src/components/Video.svelte
+++ src/components/Video.svelte
@@ -1,7 +1,8 @@
 
 <div class="w-full">
   <div class=" max-w-4xl mx-auto md:py-24 pb-10">
-    <iframe class="w-full aspect-video" src="https://www.youtube.com/embed/dWvn6nz2J1w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/756500028?h=b5c9247606&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" class="w-full aspect-video" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="1era confirmada para OCTUBRE ROJO 2022"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
   </div>
 </div>
   
+
