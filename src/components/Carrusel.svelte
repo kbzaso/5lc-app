2538ba7a7bc0a0d955101ce7037fa1eef6fafe07
Alejandro Sáez <alejandro.sapa@gmail.com>
2022-10-17 21:49:25 -0300

    Carrusel cartelera

--- src/components/Carrusel.svelte
+++ src/components/Carrusel.svelte
@@ -1,30 +1,37 @@
-<div class="carousel w-full">
-    <div id="slide1" class="carousel-item relative w-full">
-      <img src="https://placeimg.com/800/200/arch" class="w-full" />
-      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide4" class="btn btn-circle">❮</a> 
-        <a href="#slide2" class="btn btn-circle">❯</a>
-      </div>
-    </div> 
-    <div id="slide2" class="carousel-item relative w-full">
-      <img src="https://placeimg.com/800/200/arch" class="w-full" />
-      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide1" class="btn btn-circle">❮</a> 
-        <a href="#slide3" class="btn btn-circle">❯</a>
-      </div>
-    </div> 
-    <div id="slide3" class="carousel-item relative w-full">
-      <img src="https://placeimg.com/800/200/arch" class="w-full" />
-      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide2" class="btn btn-circle">❮</a> 
-        <a href="#slide4" class="btn btn-circle">❯</a>
-      </div>
-    </div> 
-    <div id="slide4" class="carousel-item relative w-full">
-      <img src="https://placeimg.com/800/200/arch" class="w-full" />
-      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide3" class="btn btn-circle">❮</a> 
-        <a href="#slide1" class="btn btn-circle">❯</a>
-      </div>
+<script>
+    const images = [
+        {
+            url: 'card/main.jpg'
+        },
+        {
+            url: 'card/alcold-marvin.jpg'
+        },
+        {
+            url: 'card/fastlove-vs-keyton-pps.jpg'
+        },
+        {
+            url: 'card/nacho-rencor-aerial.jpg'
+        },
+        {
+            url: 'card/tag.jpg'
+        }
+    ]
+</script>
+
+<div class="my-10 w-full lg:w-5/12 mx-auto">
+    <div class="my-4">
+        <h2 class="font-semibold leading-6 text-secondary uppercase text-center">CARTELERA</h2>
+        <h3 class="mt-2 text-3xl font-bold leading-8 tracking-tight text-white sm:text-4xl text-center">Octubre Rojo 2022</h3>
     </div>
-  </div>
+    <div class="carousel">
+        {#each images as image, i}
+            <div id="slide{i}" class="carousel-item relative w-full">
+                <img src={image.url} class="h-[500px] w-full object-contain" alt="Mucha gente rodeando un ring de lucha libre" />
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide{i - 1}" class="btn btn-circle">❮</a> 
+                    <a href="#slide{i + 1}" class="btn btn-circle">❯</a>
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
 	import Hero from '../components/Hero.svelte';
 	import ContentOctubre from '../components/ContentOctubre.svelte';
 	import Stats from '../components/Stats.svelte';
+	import Carrusel from '../components/Carrusel.svelte';
 </script>
 
 <svelte:head>
@@ -11,9 +12,9 @@
 <div>
 	<Hero title='¡Siempre buena onda!' subtitle='¡Vive la experiencia!' description='5 Luchas Clandestino, es un equipo humano que ve el espectáculo de la lucha libre como una herramienta de expresión creativa, enfocados en liberar el peso de la vida cotidiana de nuestra comunidad y audiencia, en una atmósfera de bohemia y sana convivencia.'></Hero>
 	<Stats />
-	<div class="container mx-auto">
-		
-	</div>
+
+		<Carrusel/>
+
 	<ContentOctubre />
 	<script async src="https://tally.so/widgets/embed.js"></script>
 </div>
