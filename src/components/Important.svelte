163724c5f0e0bacdb6177ae8b99543b99a972305
Alejandro Sáez <alejandro.sapa@gmail.com>
2022-12-19 19:11:45 -0300

    fix important component

--- src/components/Important.svelte
+++ src/components/Important.svelte
@@ -1,8 +1,8 @@
 <p class="my-4 border border-secondary p-4">
-	Antes de asistir a los eventos te pedimos favor leer nuestro <a
+	Antes de asistir a nuestros eventos te pedimos favor leer nuestro <a
 		href="https://5lc.notion.site/C-digo-de-Conducta-P-blico-B-veda-Secreta-2022-00a112b47f424470acb4b28638bfd108"
 		target="_blank"
 		class="underline decoration-secondary decoration-2"
 		>código de conducta</a
-	>
+	>.
 </p>
